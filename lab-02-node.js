8f1c0b3e37bcf219a0106e0c49681e6ae0e362a5
opyle <166080658+opyle@users.noreply.github.com>
2024-06-07 13:27:36 -0700

    Create lab-02-node.js

---
+++ lab-02-node.js
@@ -0,0 +1,13 @@
+/*
+    CIT 281 Lab 2
+    Name: Owen Pyle
+*/
+function square(num) {
+    return num*num;
+}
+console.log('Square operations:')
+for (let i = 2; i <= 10; i+=2) {
+    console.log(`Square of 20 is ${square(20)}`);
+
+}
+//testing// 
